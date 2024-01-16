59f28e8cd51c010adbccb91316bb68da1a3627a2
Vianney <vianney.deponthaud.10@gmail.com>
2024-01-16 15:08:57 +0100

    Test fonctionnement Sonar

--- src/main/java/tp_note_vdp_mn/TP_note_vdp_mn.java
+++ src/main/java/tp_note_vdp_mn/TP_note_vdp_mn.java
@@ -2,15 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.tp_note;
+package tp_note_vdp_mn;
+
+import java.util.Scanner;
 
 /**
  *
  * @author viann
  */
-public class Tp_note {
+public class TP_note_vdp_mn {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        System.out.println(i);
     }
 }
